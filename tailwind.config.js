0c7025c550feff5273f9f28e6f1c5a2ff4e46cf4
Md Mamunur Rashid <mamunkhan.mk146@gmail.com>
2025-01-05 12:47:11 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor1: '#6161FF',
+        primaryColor2: '#8039E9',
+        secondaryColor1: '#E24C4A',
+        secondaryColor2: '#9747FF',
+      },
+      fontSize:{
+        '5xl':['55px','1.1em'],
+        lg:['18px','1.2em']
+      },
+      backgroundImage: {
+        'services-1': "url('/images/advisory.png')",
+        'services-2': "url('/images/training.png')",
+        'services-3': "url('/images/managed.png')",
+        'projects': "url('/images/projects.png')",
+        'services-4': "url('/images/proService.png')",
+        'gifBg': "url('/images/gif bg.gif')",
+        'aboutUsHeroBg': "url('/images/About us Hero bg.png')",
+        'serviceHeroBg': "url('/images/service hero bg.png')",
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
