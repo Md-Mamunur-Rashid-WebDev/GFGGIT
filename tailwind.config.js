8ca63a263ac2eb3fcbf11b3b8fb3348251f37098
Md Mamunur Rashid <mamunkhan.mk146@gmail.com>
2025-01-24 16:47:48 +0600

    Adding new CDM services pages and Analytics and ai services pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ module.exports = {
       colors: {
         primaryColor1: '#3A7BD5',
         primaryColor2: '#00D2FF',
-        secondaryColor1: '#E24C4A',
-        secondaryColor2: '#9747FF',
       },
       fontSize:{
         '5xl':['55px','1.1em'],
@@ -23,6 +21,7 @@ module.exports = {
         'aboutUsHeroBg': "url('/images/About us Hero bg.png')",
         'serviceHeroBg': "url('/images/service hero bg.png')",
         'missionBg': "url('/images/mission-bg.png')",
+        'cloudServiceHeroBg': "url('/images/cloudServicesHeroBg.png')",
       }
     },
   },
